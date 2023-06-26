1576f4df846222a3ab22b6c053969593c0ac804a
Paulo Freitas <paulo.freitas.nt@gmail.com>
2023-06-26 15:53:46 -0300

    Criando método para listar

--- index.js
+++ index.js
@@ -9,16 +9,28 @@ async function conectar(){
     await client.shutdown();
 }
 
-salvar({
-    email:'maria@gmail.com',
-    nome: 'Maria'
-})
+// salvar({
+//     email:'maria@gmail.com',
+//     nome: 'Maria'
+// });
 
 async function salvar(usuario){
     await client.connect();
 
     await client.execute('INSERT INTO usuario (email,nome) VALUES (?,?) IF NOT EXISTS',
         [usuario.email, usuario.nome],{prepare:true}).then(result => console.log(result));
+
+    await client.shutdown();
+}
+
+listar();
+
+async function listar(){
+    await client.connect();
+
+    await client.execute('SELECT JSON * FROM usuario').then(result =>{
+        result.rows.forEach(r => console.log(r['[json]']));
+    });
 
     await client.shutdown();
 }
